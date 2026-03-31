b9a8205f0e356c09d81dc27a73efd3b9fb17e39f
SHxTipBot <BaLooPFinance@gmail.com>
2026-03-30 21:27:23 -0600

    Restore index.html and fix Vercel 500 error by stabilizing middleware

--- web.py
+++ web.py
@@ -35,6 +35,10 @@ app.add_middleware(
 class SecurityHeadersMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         response = await call_next(request)
+        response.headers["X-Frame-Options"] = "DENY"
+        response.headers["X-Content-Type-Options"] = "nosniff"
+        response.headers["X-XSS-Protection"] = "1; mode=block"
+        response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
         response.headers["Content-Security-Policy"] = (
             "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:; "
             "script-src * 'unsafe-inline' 'unsafe-eval' blob:; "
